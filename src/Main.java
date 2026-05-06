a9971362fd746387c07bea5f0d48efd23f7b5962
Lucía Navarro Cruz <lucnavcru@alu.edu.gva.es>
2026-05-06 10:14:32 +0200

    Menu Update. Above on customerLogin function.

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,8 @@ public class Main {
         boolean exit=false;
         while(!exit){
             System.out.println("====EMPLOYEE====");
-            System.out.println("1. ");
-            System.out.println("2. ");
-            System.out.println("3. Log out");
+            System.out.println("1. Approve a loan");
+            System.out.println("2. Log out");
         }
     }
     public static void adminLogin(Scanner sc) {
@@ -15,7 +14,7 @@ public class Main {
         while (!exit) {
             System.out.print("====ADMIN====");
             System.out.println("1. View recent transactions");
-            System.out.println("2. Approve loan");
+            System.out.println("2. Lock account");
             System.out.println("3. Unlock account");
             System.out.println("4. Log out");
             String choice = sc.nextLine();
@@ -25,8 +24,8 @@ public class Main {
                     //TODO Create a function to view recent transactions
                 break;
                 case "2":
-                    //TODO Loan approval functions
-                break;
+                    //TODO Create a function to lock an account
+                    break;
                 case "3":
                     //TODO  Unlock Account Function
                 break;
@@ -44,13 +43,33 @@ public class Main {
         while (!exit) {
             System.out.println("====CUSTOMER====");
             System.out.println("1. View recent transactions");
-            System.out.println("2. Log out");
+            System.out.println("2. Check balance");
+            System.out.println("3. Withdraw money");
+            System.out.println("4. Deposit money");
+            System.out.println("5. Make a transfer");
+            System.out.println("6. Apply for a loan");
+            System.out.println("7. Log out");
             String choice = sc.nextLine();
             switch (choice) {
                 case "1":
                     //TODO Transactions function
                 break;
                 case "2":
+                    //TODO function to check balanc
+                    break;
+                case "3":
+                    //TODO function that allows to withdraw money
+                    break;
+                case "4":
+                    //TODO function to deposit money
+                    break;
+                case "5":
+                    //TODO function to make a transfer
+                    break;
+                case "6":
+                    //TODO function to apply a loan
+                    break;
+                case "7":
                     exit = true;
                 break;
                 default:
