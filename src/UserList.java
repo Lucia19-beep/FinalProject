5a0e414c2bd751596bcc7418aab04c0a5f274cd5
Lucía Navarro Cruz <lucnavcru@alu.edu.gva.es>
2026-05-13 10:40:48 +0200

    Adding TestAddUser function to UserListTest.

--- src/UserList.java
+++ src/UserList.java
@@ -7,4 +7,17 @@ public class UserList {
     public UserList(){
         users=new HashMap<>();
     }
+    public Map<Integer, User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Map<Integer, User> users) {
+        this.users = users;
+    }
+    public void addUser(int id,User user){
+        users.put(id,user);
+    }
+    public void deleteUser(int id,User user){
+        users.remove(id,user);
+    }
 }
--- tests/UserListTest.java
+++ tests/UserListTest.java
@@ -1,5 +1,15 @@
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserListTest {
+public class UserListTest {
+    UserList userList=new UserList();
+    User user;
 
+    @Test
+    public void testAddUser(){
+        userList.addUser(1,new User("Pepe",2,"Street 5"));
+
+
+    }
 }
